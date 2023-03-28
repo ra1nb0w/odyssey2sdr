e793b0314075bdc4611575509a007072489bdd63
Davide Gerhard <rainbow@irh.it>
2023-03-28 14:52:07 +0200

    radio/protocol2: better ethernet stability with phase 157.5

--- radio/protocol2/tx_pll.v
+++ radio/protocol2/tx_pll.v
@@ -138,7 +138,7 @@ module tx_pll (
 		altpll_component.clk1_divide_by = 1,
 		altpll_component.clk1_duty_cycle = 50,
 		altpll_component.clk1_multiply_by = 1,
-		altpll_component.clk1_phase_shift = "3400",
+		altpll_component.clk1_phase_shift = "3500",
 		altpll_component.clk2_divide_by = 10,
 		altpll_component.clk2_duty_cycle = 50,
 		altpll_component.clk2_multiply_by = 1,
@@ -285,7 +285,7 @@ endmodule
 // Retrieval info: PRIVATE: PHASE_RECONFIG_FEATURE_ENABLED STRING "1"
 // Retrieval info: PRIVATE: PHASE_RECONFIG_INPUTS_CHECK STRING "1"
 // Retrieval info: PRIVATE: PHASE_SHIFT0 STRING "0.00000000"
-// Retrieval info: PRIVATE: PHASE_SHIFT1 STRING "153.00000000"
+// Retrieval info: PRIVATE: PHASE_SHIFT1 STRING "157.50000000"
 // Retrieval info: PRIVATE: PHASE_SHIFT2 STRING "0.00000000"
 // Retrieval info: PRIVATE: PHASE_SHIFT3 STRING "0.00000000"
 // Retrieval info: PRIVATE: PHASE_SHIFT_STEP_ENABLED_CHECK STRING "1"
@@ -340,7 +340,7 @@ endmodule
 // Retrieval info: CONSTANT: CLK1_DIVIDE_BY NUMERIC "1"
 // Retrieval info: CONSTANT: CLK1_DUTY_CYCLE NUMERIC "50"
 // Retrieval info: CONSTANT: CLK1_MULTIPLY_BY NUMERIC "1"
-// Retrieval info: CONSTANT: CLK1_PHASE_SHIFT STRING "3400"
+// Retrieval info: CONSTANT: CLK1_PHASE_SHIFT STRING "3500"
 // Retrieval info: CONSTANT: CLK2_DIVIDE_BY NUMERIC "10"
 // Retrieval info: CONSTANT: CLK2_DUTY_CYCLE NUMERIC "50"
 // Retrieval info: CONSTANT: CLK2_MULTIPLY_BY NUMERIC "1"
